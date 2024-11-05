b60b48b325a6eb4f23e2ba3ce756b7430fe4b619
Dahnish Chaudhri <dchaudhr@lion.lmu.edu>
2024-11-05 11:17:53 -0800

    Initial commit for backend

---
+++ lambda_function.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    
+    # connect to Dynamo resource
+    client = boto3.resource('dynamodb')
+    
+    # create a DynamoDB client to visitor_count table
+    table = client.Table('visitor_count')
+    
+    # increment visitor_count for index.html
+    response = table.update_item(
+        Key={
+            'path': 'index.html'
+        },
+        UpdateExpression="ADD visitor_count :val",
+        ExpressionAttributeValues={
+            ':val': 1
+        }
+    )
+    
+    # get visitor_count from the visitor_count table for index.html
+    response = table.get_item(
+        Key={
+            'path': 'index.html'
+        }
+    )
+    visitor_count = (response['Item']['visitor_count'])
+        
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': visitor_count
+    }
